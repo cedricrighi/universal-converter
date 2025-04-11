5954e4833dde99d911dd209d4555ff9041b7ccb0
cedricrighi <cedric.righi@gmail.com>
2025-04-11 21:01:48 +0200

    feat: start implementing routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import type { Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { ConverterComponent } from "./converter/converter.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: "",
+		component: HomeComponent,
+	},
+	{
+		path: "converter",
+		component: ConverterComponent,
+	},
+];
