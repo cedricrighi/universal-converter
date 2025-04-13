9ef761d1f7c2433e82e4b3bf1c77d863d55ded8d
cedricrighi <cedric.righi@gmail.com>
2025-04-13 11:39:36 +0200

    feat: created home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
-import { NavbarComponent } from "./navbar/navbar.component";
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-	selector: "app-root",
-	imports: [RouterOutlet, NavbarComponent],
-	templateUrl: "./app.component.html",
-	styleUrl: "./app.component.css",
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-	title = "Universal-Converter";
+  title = 'Universal-Converter';
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+// biome-ignore lint/style/useImportType: <explanation>
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  redirectToConverter(route: string) {
+    this.router.navigate([route]);
+  }
 }
